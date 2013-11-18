fc7ee13405ee93b82b15a6514c78c40543c0a420
corsen <bcchaiklin@gmail.com>
2013-11-17 19:54:00 -0500

    Adding User / Authentication
    Created a User model and added authentication via devise.  The user
    model is incomplete and was constructed with the minimum requirements
    for devise authentication to work.
    
    We need to add additional fields to the user model to achieve the application
    goals such as points.

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,8 @@ module CodeDojo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Devise tip for deploying on Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
