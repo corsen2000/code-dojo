c420331e123383ca450833a16b52ac058509e121
corsen <bcchaiklin@gmail.com>
2013-11-24 14:33:40 -0500

    Require authentication to create a submission

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,5 +1,6 @@
 class SubmissionsController < ApplicationController
   before_action :set_submission, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, only: [:new]
 
   # GET /submissions
   # GET /submissions.json
